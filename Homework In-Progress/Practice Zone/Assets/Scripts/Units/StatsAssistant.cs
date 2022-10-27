06b2d17e9e514021e120efbfbdc3ae1f31a488b6
IamGreyJacket <greyserk2712@gmail.com>
2022-10-27 18:36:05 +0400

    Added new project

---
+++ Homework In-Progress/Practice Zone/Assets/Scripts/Units/StatsAssistant.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Units
+{
+    public class StatsAssistant
+    {
+        private float _speed = 5f;
+
+        public float GetSpeed() => _speed;
+    }
+}
