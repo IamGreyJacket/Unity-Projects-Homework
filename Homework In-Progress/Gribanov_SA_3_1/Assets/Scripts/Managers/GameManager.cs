af38a4c2860e8eea600bcbf4fa99f9dcc10d338d
IamGreyJacket <greyserk2712@gmail.com>
2022-10-30 02:38:46 +0400

    Project added

---
+++ Homework In-Progress/Gribanov_SA_3_1/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arkanoid.Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField]
+        private int health = 3;
+        public static GameManager Self;
+        public WorldManager world;
+        [SerializeField]
+        private void Awake()
+        {
+            Self = this;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void ReduceHealth()
+        {
+            health -= 1;
+        }
+
+        public void GameOver()
+        {
+
+        }
+
+        public void MoveMyBall()
+        {
+            StartCoroutine(world.MoveBall());
+        }
+
+        public void ResetBall()
+        {
+            world.ResetBall();
+        }
+    }
+}
